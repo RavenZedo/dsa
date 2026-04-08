53d1fcd3625f281a365697d7eb773255e20295ec
RavenZedo <sumitjha20024@gmail.com>
2026-04-09 00:27:27 +0530

    rearrange by sign

--- #LEETCODE/rearrange_by_sign.py
+++ #LEETCODE/rearrange_by_sign.py
@@ -1,20 +1,17 @@
 #rearrange array in alternating positive and negative items
 #leetcode 2149
 
-nums = [3,1,-2,-5,2,-4]
-positive = []
-negative = []
-for num in nums:
-    if num >= 0:
-        positive.append(num)
-    else:
-        negative.append(num)
-result = []
-i = 0
-j = 0
-while i < len(positive) and j < len(negative):
-    result.append(positive[i])
-    result.append(negative[j])
-    i += 1
-    j += 1
-print(result)    
+def rearrangeArray(self, nums):
+    n = len(nums)
+    result = [0] * n
+    pos = 0
+    neg = 1
+    for i in range(n):
+        if nums[i] >= 0:
+            result[pos] = nums[i]
+            pos += 2
+        else:
+            result[neg] = nums[i]
+            neg += 2
+    return result
+ 
