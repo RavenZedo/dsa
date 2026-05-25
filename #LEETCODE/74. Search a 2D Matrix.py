205b82933e58d50c317e0c16352b169654ea0cfc
RavenZedo <sumitjha20024@gmail.com>
2026-05-25 20:55:23 +0530

    lc74

---
+++ #LEETCODE/74. Search a 2D Matrix.py
@@ -0,0 +1,20 @@
+#74. Search a 2D Matrix
+def searchMatrix(self, matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+    rows = len(matrix)
+    cols=len(matrix[0])
+    i=0
+    j=rows*cols-1
+    while i<j:
+        mid = (i+j)//2
+        row = mid // cols
+        col = mid % cols
+        if matrix[row][col]==target:
+            return True
+        elif matrix[row][col]<target:
+            i= mid+1
+        else:
+            j=mid-1
+    return False   
+    
